50c01ad9e9951c9a63b105d3b4a8d07f3e3e13c3
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2020-03-07 15:49:28 +0600

    'j

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -5,26 +5,27 @@
 				<div class="col-lg-8 col-12">
 					<!-- Contact -->
 					<ul class="content">
-						<li><i class="fa fa-phone"></i>01798032828</li>
-						<li><a href="mailto:info@yourdomain.com"><i class="fa fa-envelope-o"></i>contact@yourdomain.com</a></li>
-						<li><i class="fa fa-clock-o"></i>Opening: 10:00am - 5:00pm</li>
+						<li><b>Head Controller: </b></li>
+						<li><i class="fa fa-phone"></i>+8801798032828</li>
+						<li><i class="fa fa-envelope-o"></i>info@brittanto.com</li>
 					</ul>
 					<!-- End Contact -->
 				</div>
 				<div class="col-lg-4 col-12">
 					<!-- Social -->
 					<ul class="social">
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus "></i></a></li>
-						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="#"><i class="fa fa-behance"></i></a></li>
-						<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-						<li><a href="{{ route('login') }}">Login</a></li>
-					</ul>
-					<!-- End Social -->
-				</div>
-			</div>
-		</div>
+	                    {{-- <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-google-plus "></i></a></li>
+	                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li> --}}
+	                    <li><a href="{{ url('features') }}">Feature</a></li>
+	                    <li><a href="{{ route('login') }}">Login</a></li>
+	                </ul>
+	                <!-- End Social -->
+	            </div>
+	        </div>
+	    </div>
 	</div>
 </header>
